785379857106bea395a603eaaffc511b3994db9a
ellievi <residentamv@hotmail.com>
2022-09-16 16:21:48 -0300

    Update exercice

--- dateclass.js
+++ dateclass.js
@@ -1,10 +1,8 @@
 class DateFormat{
-    // Creation of the attribute date = new Date(_) to use later
-    date = new Date();
 
-    // Creation of the constructor as an object with info for him parametres
-    constructor(info){
-        this.date = info.date;
+    // Creation of the constructor with variables + the value new Date()
+    constructor(){
+        this.date = new Date();
     };
     
     // Creation of the methods - 1/3 -
@@ -13,7 +11,8 @@ class DateFormat{
 
     // Creation of the methods - 2/3 -
     // Add hours, minute, second, day, month or year
-    addHoursToDate = () => `${this.date.getHours + 2}`
+    addHoursToDate = () => `${this.date.getHours() + 7}`
+    addMonthToDate = () => `${this.date.getMonth() + 4}`
 
     // Creation of the methods - 3/3 -
     // Format to brazilian's date standard
@@ -21,7 +20,7 @@ class DateFormat{
 }
 
 // Creation of an obje'ct that carry the data info abt the date
-const dataChest = {Date: new Date()};
+// const dataChest = {date: new Date()};
 
 // Creation of the base instance for the others instances
 const dateFormatInstance = new DateFormat(dataChest);
@@ -30,7 +29,8 @@ const dateFormatInstance = new DateFormat(dataChest);
 const showDateToMsFormat = dateFormatInstance.formatDateToMs();
 
 // Creation of the instance - 2/3 -
-
+const showAddHours = dateFormatInstance.addHoursToDate();
+const showAddMonth = dateFormatInstance.addMonthToDate();
 
 // Creation of the instance - 3/3 -
 const showBrazilianFormat = dateFormatInstance.formatToBrazilianDate();
@@ -38,3 +38,5 @@ const showBrazilianFormat = dateFormatInstance.formatToBrazilianDate();
 // Instance's console log
 console.log(showDateToMsFormat);
 console.log(showBrazilianFormat);
+console.log(showAddHours);
+console.log(showAddMonth);
