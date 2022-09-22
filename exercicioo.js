da4017220eb93aa3f0420b295a418d304a14e9f7
ellievi <residentamv@hotmail.com>
2022-09-22 13:01:01 -0300

    Update

---
+++ exercicioo.js
@@ -0,0 +1,110 @@
+//map
+	
+// filter 
+	
+// forEach
+	
+// reduce
+	
+// forIn
+
+/**
+ * - Filtrar os:
+ *     - pares
+ *     - impares
+ *     - 2 digitos
+ *     - 3 digitos
+ *     - 2+ digitos
+ *
+ * - somar os:
+ *      - imapres
+ *      - pares
+ */
+ const numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 20, 22, 24, 26, 28, 30, 500, 5002, 4567889,
+  ];
+  /**
+   * ordernar:
+   *  - maior para menor
+   *  - menor para maior
+   * filtrar:
+   *  - vogais
+   *  - consoantes
+   *  - maiusculas
+   *  - minusculas
+   *  - minusculas + vogais
+   *  - maisuculas + consoantes
+   */
+  const letters = [
+    'y', 'b', 'z', 'D', 'e', 'f', 'g', 'h', 'i', 'j',
+    'w', 'm', 'N', 'o', 'P', 'q', 'r', 's', 't', 'u',
+    'v', 'x', 'c', 'K', 'l', 'a',
+  ];
+  /**
+   * filtrar:
+   *  - bruno, carlos
+   *  - greg
+   *  - alex, greg
+   *  - primeiro e ultimo nome
+   *  - segundo e penultimo nome
+   *  - nome do meio da colecao
+   */
+  const names = [
+    'alex', 'bruno', 'carlos', 'daniel', 'danilo', 'euler', 'greg',
+  ];
+  /**
+   * filtrar:
+   *  - somar os impares
+   *  - somar os pares
+   *  - somar os maiores que 9+
+   *  - multiplicar os impares
+   *  - Selecionar os numeros maiores que 9+
+   */
+  const numbersFloat = [
+    1.1, 2.4, 4.56, 0.4, 123.456, 3.4, 4567.8, 1.0, 10.0, 10.00, 10.000, 323.3233, 9.8765,
+  ];
+  /**
+   * Somar os numeros inteiros
+   * Somar os numeros com ponto flutuante
+   * Selecionar as letras apenas
+   * Selecionar os objetos apenas
+   * Selecionar letras minusculas
+   * Selecionar letras maisculas
+   * Selecionar numeros com pontos flutuantes
+   * Selecionar o nome "XUXA" e exibir sua idade
+   *
+   */
+  const numbersObject = [
+    { value: 1 },
+    { value: 4 },
+    { value: 56 },
+    { value: 'A' },
+    { value: 'b' },
+    { value: 'C' },
+    { value: 'd' },
+    { value: 'EEE' },
+    { value: 59 },
+    { value: 10000 },
+    { value: 5.6 },
+    { value: 98.7 },
+    { value: 'Xuxa', age: 201 },
+    { value: { name: 'Bruno' } },
+  ];
+  /**
+   * Forma uma frase com as informações do objeto a seguir
+   * Ex.: ` Ola meus nome é ${variavel}, ... `
+   */
+  const person = {
+    name: 'José',
+    age: 50,
+    hobbies: ['nadar', 'corre', 'pedala'],
+    sons: ['Mary', 'John'],
+    languages: ['portuguese', 'chinese'],
+    friends: [{
+      name: 'Marcos',
+      age: 60,
+    }, {
+      name: 'Marie',
+      age: 57,
+    }],
+  };
--- mathExercice.js
+++ mathExercice.js
@@ -3,12 +3,12 @@
 - Divisao (estatica)  OK
 - Subtração (estatica)  OK
 - Multiplicação (estatica)  OK
-- Raiz quadrada  
-- Logaritmo 
+- Raiz quadrada  OK
+- Logaritmo OK
 - Potencia  OK
-- Número de PI
-- Número de Euler
-- Somar uma lista de numeros 
+- Número de PI OK
+- Número de Euler OK
+- Somar uma lista de numeros OK
    - parametro: um array de inteiros
       ex:  usando o REDUCER do javascript de preferência
           const valores = [1,2,3,4,5,6,7, ...]
@@ -19,41 +19,58 @@ Obs:
 */
 
 class CustomMath{
-    static sum = (value1, value2) => {
-        return value1 + value2
-    }
 
-    static div = (value1, value2) => {
-        return (value1) / (value2)
-    }
+    static AUTORA = 'Elisama'
 
-    static sub = (value1, value2) => {
-        return value1 - value2
-    }
+    static sum = (value1, value2) => value1 + value2
 
-    static mul = (value1, value2) => {
-        return value1 * value2
-    }
+    static div = (value1, value2) => value1 / value2
 
-    raizqua = (value1) => {
-        return value1 * 2 / value1
-    }
+    static sub = (value1, value2) => value1 - value2
 
-    log = (value1) => {
+    static mul = (value1, value2) => value1 * value2
 
-    }
+    raizqua = (value1) =>  value1 ** (1/2)
 
-    pot = (value1, value2) => {
-        return value1 ** value2
-    }
+    log = (value1) => Math.log(value1)
+
+    pi = () => Math.PI
+
+    euler = () => Math.E
+
+    pot = (value1, value2) => value1 ** value2
+    
 }
 
 console.log('Soma: ', CustomMath.sum(1,3));
 console.log('Divisão: ', CustomMath.div(4,2));
 console.log('Subtração: ', CustomMath.sub(4,3));
 console.log('Multiplicação: ', CustomMath.mul(4,4));
+console.log('Constante da classe: ', CustomMath.AUTORA);
 
 const math = new CustomMath()
-
-console.log('Raiz quadrada: ', math.raizqua(16))
+console.log('Raiz quadrada: ', math.raizqua(25))
 console.log('Potência: ', math.pot(5, 2))
+console.log('PI: ', math.pi())
+console.log('Euler: ', math.euler())
+console.log('Logarítimo: ', math.log(20))
+
+const values = [
+    {value: "value1", quant: 1},
+    {value: "value2", quant: 2},
+    {value: "value3", quant: 3},
+    {value: "value1", quant: 4},
+    {value: "value2", quant: 5},
+    {value: "value3", quant: 6},
+    {value: "value2", quant: 7},
+    {value: "value3", quant: 8},
+    {value: "value1", quant: 9},
+    {value: "value2", quant: 10}
+]
+const totalValues = values.reduce( (acumulator, next) => acumulator + next.quant, 0)
+
+const totalValues1 = values.reduce( function (acumulator, next) {
+    return acumulator + next.quant} , 0)
+
+console.log('Reduce', totalValues)
+console.log('Reduce2', totalValues1)
